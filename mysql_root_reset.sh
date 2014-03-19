17f4a0315daeb1d97db2706b8aa49a22703d6202
TAHMI Hamza <tahmi.hamza@gmail.com>
2014-03-19 11:37:55 +0100

    mysql root password reset

---
+++ mysql_root_reset.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+new_password=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c12`
+#check if uid=0
+if [ "$(id -u)" != "0" ]; then
+   echo "run it as root / use sudo " 1>&2
+   exit 1
+fi
+
+#stop mysql 
+/etc/init.d/mysql stop
+
+#run it again with  --skip-grant-tables
+/usr/bin/mysqld_safe  --skip-grant-tables > /dev/null 2>&1 &
+
+#change the root password 
+mysql -uroot mysql -e "UPDATE mysql.user SET Password=PASSWORD('$new_password') WHERE User='root';"
+
+#shtudown mysql
+/usr/bin/mysqladmin shutdown
+
+#start again 
+/etc/init.d/mysql start
+
+echo "your new password is : $new_password"
+
